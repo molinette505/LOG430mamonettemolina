1310c67bc2b73df89c23eaf9f2bf9d8f29b4fdd1
Marc-Alexandre Monette Molina <marc-alexandre.monette-molina.1@ens.etsmtl.ca>
2025-05-19 19:25:07 +0000

    changed addition test to trigger CICD pipeline

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,8 +1,8 @@
 import { add, factorial } from '../src/utils';
 
 // Test 1 : Addition simple
-test('add(2, 3) should return 5', () => {
-    expect(add(2, 3)).toBe(5);
+test('add(2, 4) should return 6', () => {
+    expect(add(2, 4)).toBe(6);
 });
 
 // Test 2 : Factorielle de 4
